da43fd0cb8d281ac860923127849db9abd223aa9
Emre Ozturk <emreeozturk42@gmail.com>
2023-11-26 02:01:00 +0300

    boots user service

--- apps/users/src/users.service.ts
+++ apps/users/src/users.service.ts
@@ -1,8 +1,54 @@
+import { JwtService } from '@nestjs/jwt';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { LoginDto, RegisterDto } from './dto/user.dto';
 
 @Injectable()
 export class UsersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly jwtService: JwtService,
+    // private readonly prisma : PrismaSer
+    private readonly configService: ConfigService,
+  ) {}
+
+  // register service
+
+  async register(registerDto: RegisterDto) {
+    const { name, email, password } = registerDto;
+    // const hashedPassword = await bcrypt.hash(password, 10);
+    // const user = await this.prisma.user.create({
+    //   data: {
+    //     name,
+    //     email,
+    //     password: hashedPassword,
+    //   },
+    // });
+    const user = {
+      name,
+      email,
+      password,
+    };
+    return user;
+  }
+
+  // login service
+
+  async login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+    // const user = await this.prisma.user.findUnique({ where: { email } });
+    const user = {
+      email,
+      password,
+    };
+
+    return user;
+  }
+
+  // get all users service
+
+  async getAllUsers() {
+    // const users = await this.prisma.user.findMany();
+    const users = [];
+    return users;
   }
 }
