e522c0cb75842d8a4c43fc4af5bda98db0a0b0a6
Emre Ozturk <emreeozturk42@gmail.com>
2023-11-27 03:37:09 +0300

    added mail service

--- apps/users/src/email/email.module.ts
+++ apps/users/src/email/email.module.ts
@@ -1,7 +1,27 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { EmailService } from './email.service';
-
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigService } from '@nestjs/config';
+@Global()
 @Module({
+  imports: [
+    MailerModule.forRootAsync({
+      useFactory: async (config: ConfigService) => ({
+        transport: {
+          host: config.get<string>('SMPT_HOST'),
+          port: config.get('MAIL_PORT'),
+          secure: true,
+          auth: {
+            user: config.get<string>('SMTP_USER'),
+            pass: config.get<string>('SMTP_PASSWORD'),
+          },
+        },
+        defaults: {
+          from: 'Foodie',
+        },
+      }),
+    }),
+  ],
   providers: [EmailService],
 })
 export class EmailModule {}
